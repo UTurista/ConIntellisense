9dcb37a9c4c4fbfe78f3ddc5519f995d0b4b5e66
UTurista <vasco@programador.pt>
2021-06-21 13:13:58 +0100

    Updates the text document handler

--- LanguageServer/Src/Handler/TextDocumentSyncHandler.cs
+++ LanguageServer/Src/Handler/TextDocumentSyncHandler.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using ConIntellisense.Server.Service;
+using MediatR;
+using Microsoft.Extensions.Logging;
 using OmniSharp.Extensions.LanguageServer.Protocol;
 using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
 using OmniSharp.Extensions.LanguageServer.Protocol.Document;
@@ -16,6 +18,8 @@ namespace ConIntellisense.Server.Handler
     public class TextDocumentSyncHandler : ITextDocumentSyncHandler
     {
         private readonly ILanguageServerFacade _router;
+        private readonly ITreeManager treeManager;
+        private readonly ILogger<TextDocumentSyncHandler> logger;
         private readonly TextDocumentSyncKind change  = TextDocumentSyncKind.Full;
 
         private readonly DocumentSelector _documentSelector = new(
@@ -27,9 +31,12 @@ namespace ConIntellisense.Server.Handler
 
         private SynchronizationCapability _capability;
 
-        public TextDocumentSyncHandler(ILanguageServerFacade router)
+        public TextDocumentSyncHandler(ILogger<TextDocumentSyncHandler> logger, ILanguageServerFacade router, ITreeManager treeManager)
         {
             _router = router;
+            this.treeManager = treeManager;
+            this.logger = logger;
+            logger.LogInformation("hello Workd!!!");
         }
 
 
@@ -46,23 +53,54 @@ namespace ConIntellisense.Server.Handler
             };
         }
 
+
+        TextDocumentOpenRegistrationOptions IRegistration<TextDocumentOpenRegistrationOptions, SynchronizationCapability>.GetRegistrationOptions(SynchronizationCapability capability, ClientCapabilities clientCapabilities)
+        {
+            return new TextDocumentOpenRegistrationOptions()
+            {
+                DocumentSelector = _documentSelector,
+            };
+        }
+
+        TextDocumentCloseRegistrationOptions IRegistration<TextDocumentCloseRegistrationOptions, SynchronizationCapability>.GetRegistrationOptions(SynchronizationCapability capability, ClientCapabilities clientCapabilities)
+        {
+            return new TextDocumentCloseRegistrationOptions()
+            {
+                DocumentSelector = _documentSelector,
+            };
+        }
+
+
+        TextDocumentSaveRegistrationOptions IRegistration<TextDocumentSaveRegistrationOptions, SynchronizationCapability>.GetRegistrationOptions(SynchronizationCapability capability, ClientCapabilities clientCapabilities)
+        {
+            return new TextDocumentSaveRegistrationOptions()
+            {
+                DocumentSelector = _documentSelector,
+            };
+        }
+
+        public Task<Unit> Handle(DidOpenTextDocumentParams request, CancellationToken cancellationToken)
+        {
+            var documentPath = request.TextDocument.Uri.ToString();
+            var text = request.TextDocument.Text;
+            treeManager.UpdateTree(documentPath, text);
+
+            return Unit.Task;
+        }
+
         public Task<Unit> Handle(DidChangeTextDocumentParams request, CancellationToken cancellationToken)
         {
             var documentPath = request.TextDocument.Uri.ToString();
             var text = request.ContentChanges.FirstOrDefault()?.Text;
 
-            // _bufferManager.UpdateBuffer(documentPath, new StringBuffer(text));
+            treeManager.UpdateTree(documentPath, text);
 
             _router.Window.LogInfo($"Updated buffer for document: {documentPath}\n{text}");
 
             return Unit.Task;
         }
 
-        public Task<Unit> Handle(DidOpenTextDocumentParams request, CancellationToken cancellationToken)
-        {
-            // _bufferManager.UpdateBuffer(request.TextDocument.Uri.ToString(), new StringBuffer(request.TextDocument.Text));
-            return Unit.Task; ;
-        }
+      
         public Task<Unit> Handle(DidSaveTextDocumentParams request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
@@ -72,20 +110,6 @@ namespace ConIntellisense.Server.Handler
         {
             throw new NotImplementedException();
         }
-        TextDocumentOpenRegistrationOptions IRegistration<TextDocumentOpenRegistrationOptions, SynchronizationCapability>.GetRegistrationOptions(SynchronizationCapability capability, ClientCapabilities clientCapabilities)
-        {
-            throw new NotImplementedException();
-        }
 
-        TextDocumentCloseRegistrationOptions IRegistration<TextDocumentCloseRegistrationOptions, SynchronizationCapability>.GetRegistrationOptions(SynchronizationCapability capability, ClientCapabilities clientCapabilities)
-        {
-            throw new NotImplementedException();
-        }
-
-
-        TextDocumentSaveRegistrationOptions IRegistration<TextDocumentSaveRegistrationOptions, SynchronizationCapability>.GetRegistrationOptions(SynchronizationCapability capability, ClientCapabilities clientCapabilities)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
